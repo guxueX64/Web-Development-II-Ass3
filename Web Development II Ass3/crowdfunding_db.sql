501d5b22a6f3198610698de5c1b45bb1a1ffa0ca
guxueX64 <wzy88335256@126.com>
2024-10-13 22:58:58 +0800

    update html

--- Web Development II Ass3/crowdfunding_db.sql
+++ Web Development II Ass3/crowdfunding_db.sql
@@ -1,5 +1,4 @@
 
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -19,6 +18,31 @@ CREATE TABLE `category`  (
 INSERT INTO `category` VALUES (1, 'Medical');
 INSERT INTO `category` VALUES (2, 'Memorial');
 INSERT INTO `category` VALUES (3, 'Family');
+INSERT INTO `category` VALUES (4, 'education');
+INSERT INTO `category` VALUES (5, 'service');
+
+-- ----------------------------
+-- Table structure for donation
+-- ----------------------------
+
+CREATE TABLE `donation`  (
+  `donation_id` int(11) NOT NULL AUTO_INCREMENT,
+  `amount` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '金额',
+  `giver` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '捐赠人',
+  `fundraiser_id` int(11) NOT NULL COMMENT '筹款人id',
+  `date` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '捐赠时间',
+  PRIMARY KEY (`donation_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of donation
+-- ----------------------------
+INSERT INTO `donation` VALUES (3, '134', 'test', 1, '2024-10-9 21:51:59');
+INSERT INTO `donation` VALUES (4, '12', 'www', 1, '2024-10-9 21:52:8');
+INSERT INTO `donation` VALUES (5, '188', 'zhangsan', 1, '2024-10-10 16:53:31');
+INSERT INTO `donation` VALUES (6, '156', 'lisi', 1, '2024-10-10 16:53:53');
+INSERT INTO `donation` VALUES (7, '15', 'lala', 11, '2024-10-10 16:55:0');
+INSERT INTO `donation` VALUES (8, '16', 'newone', 11, '2024-10-10 16:55:13');
 
 -- ----------------------------
 -- Table structure for fundraiser
@@ -35,17 +59,17 @@ CREATE TABLE `fundraiser`  (
   `category_id` int(11) NULL DEFAULT NULL COMMENT '类别id',
   `fundraiser_image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '图片',
   PRIMARY KEY (`fundraiser_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of fundraiser
 -- ----------------------------
-INSERT INTO `fundraiser` VALUES (1, 'Firefighters Local 3631', 'Fundraiser for OCFA Handcrew', '500,000', '223,782', 'Chicago', '1', 1, '1.png');
-INSERT INTO `fundraiser` VALUES (2, 'Toni Ocallaghan', 'Saving Jay', '250,000', '29,695', 'Los Angeles', '1', 2, '2.png');
-INSERT INTO `fundraiser` VALUES (3, 'Nico Pellatz', 'Neustart für Berkin', '2,000,000', '259,928', 'Seattle', '1', 3, '3.png');
-INSERT INTO `fundraiser` VALUES (4, 'Roxanne De Groof', 'Help mij het leven nog wat langer te vieren.', '950,000', '112,785', 'Los Angeles', '2', 1, '4.png');
-INSERT INTO `fundraiser` VALUES (5, 'Gina DellaMonica- Giacchetto and 2 others', 'Join us to help Austin and Bella Trezza and their family', '200,000', '158,835', 'Chicago', '1', 3, '5.png');
-INSERT INTO `fundraiser` VALUES (6, 'Corneby and Titmus Family and Friends and Rob Corneby', 'Help a Family in Need on their Journey to Healing', '100,000', '176,465', 'Los Angeles', '2', 3, '6.png');
-INSERT INTO `fundraiser` VALUES (7, 'Claudia Elsässer is organizing', 'Plötzlicher Tod des Familienvaters', '600,000', '50,750', 'Los Angeles', '2', 1, '7.png');
+INSERT INTO `fundraiser` VALUES (1, 'OC Firefighters Local 3631', 'Fundraiser for OCFA Handcrew', '500000', '490', 'Chicago', '1', 1, '1.png');
+INSERT INTO `fundraiser` VALUES (2, 'Toni Ocallaghan', 'Saving Jay', '250000', '0', 'Los Angeles', '1', 2, '2.png');
+INSERT INTO `fundraiser` VALUES (3, 'Nico Pellatz', 'Neustart für Berkin', '2000000', '0', 'Seattle', '1', 3, '3.png');
+INSERT INTO `fundraiser` VALUES (4, 'Roxanne De Groof', 'Help mij het leven nog wat langer te vieren.', '95000', '0', 'Los Angeles', '2', 1, '4.png');
+INSERT INTO `fundraiser` VALUES (5, 'Gina DellaMonica- Giacchetto and 2 others', 'Join us to help Austin and Bella Trezza and their family', '200000', '0', 'Chicago', '1', 3, '5.png');
+INSERT INTO `fundraiser` VALUES (6, 'Corneby and Titmus Family and Friends and Rob Corneby', 'Help a Family in Need on their Journey to Healing', '100000', '0', 'Los Angeles', '2', 3, '6.png');
+INSERT INTO `fundraiser` VALUES (7, 'Claudia Elsässer is organizing', 'Plötzlicher Tod des Familienvaters', '60000', '0', 'Los Angeles', '2', 1, '7.png');
 
 SET FOREIGN_KEY_CHECKS = 1;
